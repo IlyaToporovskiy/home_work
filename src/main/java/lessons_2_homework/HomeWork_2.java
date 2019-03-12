7e9e41e8ec97e303dd13d39b8a66ff6641ef23ee
IlyaToporovskiy <lelikleti0@gmail.com>
2019-03-12 14:10:02 +0300

    решение дз_2 задача с 1 по 3

---
+++ src/main/java/lessons_2_homework/HomeWork_2.java
@@ -0,0 +1,63 @@
+package lessons_2_homework;
+
+public class HomeWork_2 {
+  public static void main(String[] args) {
+
+    //// Задача 1
+
+//    System.out.println("Задача 1 \n");
+//    Scanner sc1 = new Scanner(System.in);
+//    Scanner sc2 = new Scanner(System.in);
+//    Scanner sc3 = new Scanner(System.in);
+//    System.out.println("Введите число a :");
+//    int a = sc1.nextInt();
+//    System.out.println("Введите число b :");
+//    int b = sc2.nextInt();
+//    System.out.println("Введите число c :");
+//    int c = sc3.nextInt();
+//
+//    if (a > b & a > c) {
+//      if (b > c) {
+//        System.out.println("Последовательность в порядке возрастания = " + c + " " + b + " " + a);
+//      } else {
+//        System.out.println("Последовательность в порядке возрастания = " + b + " " + c + " " + a);
+//      }
+//    } else if (b > a & b > c) {
+//      if (a > c) {
+//        System.out.println("Последовательность в порядке возрастания = " + c + " " + a + " " + b);
+//      } else {
+//        System.out.println("Последовательность в порядке возрастания = " + a + " " + c + " " + b);
+//      }
+//    } else if (c > a & c > b) {
+//      if (a > b) {
+//        System.out.println("Последовательность в порядке возрастания = " + b + " " + a + " " + c);
+//      } else {
+//        System.out.println("Последовательность в порядке возрастания = " + a + " " + b + " " + c);
+//      }
+//    }else {
+//      System.out.println("Введите правильно цифры !");
+//    }
+
+    //// Задача 2
+//    System.out.println("\n Задача 2 \n");
+//    int num2=90;
+//    do {
+//      System.out.println("число = "+num2);
+//      num2-=5;
+//    }while (num2>=0);
+
+
+//    // Задача 3
+//    System.out.println("\n Задача 3 \n");
+//    int num3=20;
+//    //Math.pow(b,2)
+//    for (int i=1;i<21;i++){
+//      System.out.println("Элемент последовательности " +i+ " равен = "+(int)Math.pow(2,i));
+//    }
+
+//       //Задача 4  проверить является ли натуральное число простым
+    System.out.println("\n Задача 4 \n");
+
+
+  }
+}
