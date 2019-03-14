60639d823bca291f7228ea5f85ee15a562004e2e
IlyaToporovskiy <lelikleti0@gmail.com>
2019-03-14 22:18:06 +0300

    домашка 3 задача 1

---
+++ src/main/java/lessons_3_homework/Zadacha_1.java
@@ -0,0 +1,28 @@
+package lessons_3_homework;
+
+import java.util.Arrays;
+
+public class Zadacha_1 {
+    public static void main(String[] args) {
+        System.out.println("Задача 1 ");
+        int count=0;
+        for(int i=1,j=0 ; j<=20;i++){
+            j=2*i;
+            count++;
+        }
+        count--;
+        System.out.println("Длина массива = "+count);
+        int [] arr1 =new int [count];
+        for(int i=0;i<(arr1.length);i++){
+            arr1[i]=i*2+2;
+        }
+        System.out.println("Выводим элемент массива в строку через пробел "+
+                Arrays.toString(arr1).replaceAll(", "," "));
+        StringBuilder stringBuilder =new StringBuilder();
+        for(int i=0 ;i<arr1.length;i++){
+            stringBuilder.append(arr1[i]).append("\n");
+        }
+        System.out.print(stringBuilder);
+
+    }
+}
