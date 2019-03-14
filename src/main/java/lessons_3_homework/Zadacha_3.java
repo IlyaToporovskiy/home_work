ad98a1e88a725f0723465170ceff0fede0eb5f3a
IlyaToporovskiy <lelikleti0@gmail.com>
2019-03-14 23:41:27 +0300

    домашка 3 задача 3

---
+++ src/main/java/lessons_3_homework/Zadacha_3.java
@@ -0,0 +1,23 @@
+package lessons_3_homework;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Zadacha_3 {
+    public static void main(String[] args) {
+        int num=15,count=0;
+        Random random =new Random();
+        int [] arr1 =new int[num];
+
+        for (int i = 0; i<num;i++){
+            arr1[i]=random.nextInt(10);
+        }
+        System.out.println(Arrays.toString(arr1));
+        for(int i=0;i<arr1.length;i++){
+            if(arr1[i]%2==0){
+                count++;
+            }
+        }
+        System.out.println("Количество четных = "+ count);
+    }
+}
