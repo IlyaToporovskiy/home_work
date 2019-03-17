fe5fdf04e7d9dd7512b62a3fa3e44fb20c77a0a5
IlyaToporovskiy <lelikleti0@gmail.com>
2019-03-17 23:51:38 +0300

    задача 1 урок 4

---
+++ src/main/java/lessons_4_homework/zadacha_1/ListElement.java
@@ -0,0 +1,6 @@
+package lessons_4_homework.zadacha_1;
+
+public class ListElement {
+    ListElement next;    // указатель на следующий элемент
+    int data;            // данные
+}
---
+++ src/main/java/lessons_4_homework/zadacha_1/Main.java
@@ -0,0 +1,19 @@
+package lessons_4_homework.zadacha_1;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        List list_1 = new List();
+        list_1.addFront(1);
+        list_1.addFront(2);
+        list_1.addFront(3);
+
+
+        list_1.addBack(4);
+        list_1.addBack(5);
+        list_1.addBack(6);
+//
+        list_1.printList();
+    }
+}
