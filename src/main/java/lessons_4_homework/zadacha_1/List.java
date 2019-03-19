a3f83693bc8eb5265117bc40d521aa072416f2ea
IlyaToporovskiy <lelikleti0@gmail.com>
2019-03-19 23:25:00 +0300

    задача 1 урок 4

--- src/main/java/lessons_4_homework/zadacha_1/List.java
+++ src/main/java/lessons_4_homework/zadacha_1/List.java
@@ -1,75 +1,97 @@
 package lessons_4_homework.zadacha_1;
 
 public class List {
-  private ListElement head;       // указатель на первый элемент
-  private ListElement tail;       // указатель последний элемент
+    private ListElement start; //ссылка на начальный элемент
+    private ListElement end; //ссылка на конец элемент
 
-  void addFront(int data)           //добавить спереди
-  {
-    ListElement a = new ListElement();  //создаём новый элемент
-    a.data = data;              //инициализируем данные.
-    // указатель на следующий элемент автоматически инициализируется как null
-    if (head == null)            //если список пуст
-    {                           //то указываем ссылки начала и конца на новый элемент
-      head = a;               //т.е. список теперь состоит из одного элемента
-      tail = a;
-    } else {
-      a.next = head;          //иначе новый элемент теперь ссылается на "бывший" первый
-      head = a;               //а указатель на первый элемент теперь ссылается на новый элемент
-    }
-  }
 
-  void addBack(int data) {          //добавление в конец списка
-    ListElement a = new ListElement();  //создаём новый элемент
-    a.data = data;
-    if (tail == null)           //если список пуст
-    {                           //то указываем ссылки начала и конца на новый элемент
-      head = a;               //т.е. список теперь состоит из одного элемента
-      tail = a;
-    } else {
-      tail.next = a;          //иначе "старый" последний элемент теперь ссылается на новый
-      tail = a;               //а в указатель на последний элемент записываем адрес нового элемента
-    }
-  }
-
-  void printList()                //печать списка
-  {
-    ListElement t = head;       //получаем ссылку на первый элемент
-    while (t != null)           //пока элемент существуе
-    {
-      System.out.print(t.data + " "); //печатаем его данные
-      t = t.next;                     //и переключаемся на следующий
-    }
-  }
-
-  void delEl(int data)          //удаление элемента
-  {
-    if (head == null)        //если список пуст -
-      return;             //ничего не делаем
-
-    if (head == tail) {     //если список состоит из одного элемента
-      head = null;        //очищаем указатели начала и конца
-      tail = null;
-      return;             //и выходим
-    }
-
-    if (head.data == data) {    //если первый элемент - тот, что нам нужен
-      head = head.next;       //переключаем указатель начала на второй элемент
-      return;                 //и выходим
-    }
-
-    ListElement t = head;       //иначе начинаем искать
-    while (t.next != null) {    //пока следующий элемент существует
-      if (t.next.data == data) {  //проверяем следующий элемент
-        if (tail == t.next)      //если он последний
-        {
-          tail = t;           //то переключаем указатель на последний элемент на текущий
+    public void addElementBack(int data) {
+        ListElement t = new ListElement();
+        t.setVal(data);
+        if (start == null) { // если список у нас
+            setStart(t);
+            setEnd(t);
+        } else if (start != null) {
+            t.setNext(getStart());
+            t.setPrev(null);
+            setStart(t);
+            t.getNext().setPrev(t);
         }
-        t.next = t.next.next;   //найденный элемент выкидываем
-        return;                 //и выходим
-      }
-      t = t.next;                //иначе ищем дальше
     }
 
-  }
+    public void addElementForward(int data) {
+        ListElement t = new ListElement();
+        t.setVal(data);
+        if (start == null) { // если список у нас
+            setStart(t);
+            setEnd(t);
+        } else if (start != null) {
+            t.setNext(null);
+            t.setPrev(getEnd());
+            setEnd(t);
+            t.getPrev().setNext(t);
+        }
+    }
+
+
+    public void removeBack() {
+        if (start != null) { // если список у нас
+            setStart(start.getNext());
+            start.setPrev(null);
+       } else if (start == null) {
+                return;
+        }
+    }
+
+    public void removeItemAll(int data){
+         if (start == null) {
+            return;
+        }else if (start.getVal() == data){
+             setStart(start.getNext());
+             start.setPrev(null);
+         }
+         ListElement t=start;
+         boolean dataHere;
+         while (t.getNext()!=null){
+             if(t.getVal()==data){
+                 //удаляем элемент
+                 t.getNext().setPrev(t.getPrev());
+                 t.getPrev().setNext(t.getNext());
+                 dataHere=true;
+             }
+             t=t.getNext();
+         }
+         if(dataHere=true){
+             System.out.println("Удалили число");
+         }
+
+    }
+
+
+
+
+    public void show(){
+        ListElement t = start;
+        while (t!=null){
+            System.out.print(t.getVal()+ " ");
+            t=t.getNext();
+        }
+    }
+
+
+    public ListElement getStart() {
+        return start;
+    }
+
+    public void setStart(ListElement start) {
+        this.start = start;
+    }
+
+    public ListElement getEnd() {
+        return end;
+    }
+
+    public void setEnd(ListElement end) {
+        this.end = end;
+    }
 }
