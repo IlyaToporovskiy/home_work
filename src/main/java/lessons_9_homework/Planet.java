e9b644f071193a820e341286cb49255cfedc1c7b
IlyaToporovskiy <lelikleti0@gmail.com>
2019-03-29 10:24:47 +0300

    решение

--- src/main/java/lessons_9_homework/Planet.java
+++ src/main/java/lessons_9_homework/Planet.java
@@ -1,113 +1,87 @@
 package lessons_9_homework;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
 //1. Написать enum, который перечисляет планеты Солнечной системы и возвращает массу планеты, ее радиус и радиус орбиты.
 public class Planet {
-    enum Planets{
-        SUN,MERCURY,VENERUS,EARTH,MARS,JUPITER,SATURN,URANUS,NEPTUN;
+  enum SolarySystem{
+    SUN("Солнце",465461,21213,6566),
+    MERCURY("Венера",32121,321321,231),
+    VENUS("МАРС",123321,1321,321),
+    EARTH("Земля",131456,6464,546),
+    MARS("Марс",4987,4679,987),
+    JUPITER("Юпитер",13211,5679,46546),
+    SATURN("Сатурн",451313,13213,87879),
+    URANUS("Уран",544846587,8898,46565),
+    NEPTUNE("Нептун",1233211,79,98);
 
-    public static Planets getInfo(Planets namePlanet){
-        for (Planets pl: values()){
-            if(namePlanet==pl){
-                return pl;
-            }
-        }
 
-        throw new AssertionError("Wrong namePlanet: " );
-    }
-    }
-
+    private String name;
     private int weightPlanet;
     private int radiusPlanet;
     private int radiusOrbitPlanet;
-    private Planets namePlanet;
 
-    public Planet(int weightPlanet, int radiusPlanet, int radiusOrbitPlanet, Planets namePlanet) {
-        this.weightPlanet = weightPlanet;
-        this.radiusPlanet = radiusPlanet;
-        this.radiusOrbitPlanet = radiusOrbitPlanet;
-        this.namePlanet = namePlanet;
+    SolarySystem(String name, int weightPlanet, int radiusPlanet, int radiusOrbitPlanet) {
+      this.name = name;
+      this.weightPlanet = weightPlanet;
+      this.radiusPlanet = radiusPlanet;
+      this.radiusOrbitPlanet = radiusOrbitPlanet;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    @Override
+    public String toString() {
+      return "SolarySystem{" +
+              "name='" + name + '\'' +
+              ", weightPlanet=" + weightPlanet +
+              ", radiusPlanet=" + radiusPlanet +
+              ", radiusOrbitPlanet=" + radiusOrbitPlanet +
+              '}';
+    }
+
+    public void setName(String name) {
+      this.name = name;
     }
 
     public int getWeightPlanet() {
-        return weightPlanet;
+      return weightPlanet;
     }
 
     public void setWeightPlanet(int weightPlanet) {
-        this.weightPlanet = weightPlanet;
+      this.weightPlanet = weightPlanet;
     }
 
     public int getRadiusPlanet() {
-        return radiusPlanet;
+      return radiusPlanet;
     }
 
     public void setRadiusPlanet(int radiusPlanet) {
-        this.radiusPlanet = radiusPlanet;
+      this.radiusPlanet = radiusPlanet;
     }
 
     public int getRadiusOrbitPlanet() {
-        return radiusOrbitPlanet;
+      return radiusOrbitPlanet;
     }
 
     public void setRadiusOrbitPlanet(int radiusOrbitPlanet) {
-        this.radiusOrbitPlanet = radiusOrbitPlanet;
+      this.radiusOrbitPlanet = radiusOrbitPlanet;
     }
+  }
 
-    public Planets getNamePlanet() {
-        return namePlanet;
+  public static void getInfo(String namePlanet){
+    for(SolarySystem planet:SolarySystem.values()){
+      if(planet.getName()==namePlanet){
+        System.out.println(planet);
+      }
     }
+  }
+  public static void main(String[] args) {
+    getInfo("Солнце");
 
-    public void setNamePlanet(Planets namePlanet) {
-        this.namePlanet = namePlanet;
-    }
+    for(SolarySystem planet:SolarySystem.values()){
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Planet planet = (Planet) o;
-        return weightPlanet == planet.weightPlanet &&
-                radiusPlanet == planet.radiusPlanet &&
-                radiusOrbitPlanet == planet.radiusOrbitPlanet &&
-                namePlanet == planet.namePlanet;
-    }
+        System.out.println(planet);
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(weightPlanet, radiusPlanet, radiusOrbitPlanet, namePlanet);
-    }
+  }
 
-    public static void main(String[] args) {
-        new PlanetObject();
-
-
-    }
-}
-class PlanetObject{
-    public static List<Planet> create(){
-        List<Planet> solarySystem =new ArrayList<>(9);
-        solarySystem.add(
-                new Planet(1334,424,4242, Planet.Planets.SUN)
-        );solarySystem.add(
-                new Planet(3132,312,534, Planet.Planets.MERCURY)
-        );solarySystem.add(
-                new Planet(7675,657,57, Planet.Planets.JUPITER)
-        );solarySystem.add(
-                new Planet(75,765,36, Planet.Planets.EARTH)
-        );solarySystem.add(
-                new Planet(1334,424,4242, Planet.Planets.NEPTUN)
-        );solarySystem.add(
-                new Planet(1334,424,4242, Planet.Planets.MARS)
-        );solarySystem.add(
-                new Planet(1334,424,4242, Planet.Planets.SATURN)
-        );solarySystem.add(
-                new Planet(1334,424,4242, Planet.Planets.VENERUS)
-        );solarySystem.add(
-                new Planet(1334,424,4242, Planet.Planets.URANUS)
-        );
-        return solarySystem;
-    }
-}
+}}
