2a77f3d029c81a3a1f125f97fd2559c93fee699b
IlyaToporovskiy <lelikleti0@gmail.com>
2019-04-04 21:05:10 +0300

    урок 9 задача 5

--- src/main/java/lessons_9_homework/Five.java
+++ src/main/java/lessons_9_homework/Five.java
@@ -10,32 +10,43 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Five {
-  public static void main(String[] args) {
-    System.out.println(getArray(3));
-  }
+    public static void main(String[] args) {
+//        System.out.println(getArray(3));
 
-  public static List<String> getArray(int length) {
-    System.out.println("Введите строки ");
-    List<String> str = new ArrayList<>(length);
-    Scanner sc = new Scanner(System.in);
-    for (int i = 0; i < length; i++) {
-      str.add(sc.nextLine());
+
+        List<String> qw =getArray(4);
+        checkSortedList(qw);
     }
-    return str;
-  }
 
-  //Определять, является ли список упорядоченным по возрастанию длины строки.
+    public static List<String> getArray(int length) {
+        System.out.println("Введите строки ");
+        List<String> str = new ArrayList<>(length);
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < length; i++) {
+            str.add(sc.nextLine());
+        }
+        return str;
+    }
+
+    //Определять, является ли список упорядоченным по возрастанию длины строки.
 //В случае отрицательного ответа выводить на экран номер первого элемента, нарушающего такую упорядоченность.
-  public static String checkSortedList(List<String> list) {
-    boolean isSort;
-    for (int i = 0; i < list.size() - 1; i++) {
+    public static void checkSortedList(List<String> list) {
+        String s = null;
+        outer:
+        for (int i = 0; i < list.size() - 1; i++) {
 
-      if (list.get(i + 1).length() - list.get(i).length() >= 0) {
-        isSort = true;
-      }
+            if (list.get(i + 1).length() - list.get(i).length() < 0) {
+                System.out.println("Номер элемента = " + i);
+                s = list.get(i);
+                System.out.println(s);
+                break outer;
+            }
+
+        }
+        if(s==null){
+            System.out.println("упорядочен по возрастанию");
+        }
+
 
     }
-    String s = new String("ответ");
-    return s;
-  }
 }
